9e04706fad67717702314a370c3b68a225ff2a7a
Venelin Krustev <venelin.v.krustev@gmail.com>
2026-05-08 14:46:20 +0300

    fix: dispose driver after test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,15 +1,30 @@
-﻿namespace SeleniumBasics;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumBasics;
 
 public class Tests
 {
+    private IWebDriver driver;
+
     [SetUp]
     public void Setup()
     {
+        driver = new ChromeDriver();
     }
 
     [Test]
-    public void Test1()
+    public void GoogleTitleIsCorrect()
     {
-        Assert.Pass();
+        driver.Navigate().GoToUrl("https://www.google.com");
+        string title = driver.Title;
+        Assert.That(title, Does.Contain("Google"));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Quit();
+        driver.Dispose();
     }
 }
