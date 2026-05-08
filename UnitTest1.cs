bc24f9662770049dbdeeeb83cbf782c99c637f6a
Venelin Krustev <venelin.v.krustev@gmail.com>
2026-05-08 18:01:48 +0300

    feat: add Google search test with WebDriverWait and JS executor

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,6 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumBasics;
 
@@ -19,6 +20,22 @@ public class Tests
         driver.Navigate().GoToUrl("https://www.google.com");
         string title = driver.Title;
         Assert.That(title, Does.Contain("Google"));
+    }
+
+    [Test]
+    public void GoogleSearchReturnsResults()
+    {
+        driver.Navigate().GoToUrl("https://www.google.com");
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement searchBox = wait.Until(d => {
+            var el = d.FindElement(By.CssSelector("textarea[name='q']"));
+            return el.Displayed && el.Enabled ? el : null;
+        });
+        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+        js.ExecuteScript("arguments[0].value='Selenium C#';", searchBox);
+        searchBox.Submit();
+        wait.Until(d => d.Title.Contains("Selenium"));
+        Assert.That(driver.Title, Does.Contain("Selenium"));
     }
 
     [TearDown]
